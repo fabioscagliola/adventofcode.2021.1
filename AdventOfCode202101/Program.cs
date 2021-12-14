4fec732f22a3156c52231ecde01e66f007ce659e
Fabio Scagliola <scagliola@gmail.com>
2021-12-14 13:59:46 +0100

    Both parts of this puzzle are complete!

---
+++ AdventOfCode202101/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace com.fabioscagliola.AdventOfCode202101
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] lineList = File.ReadAllLines("Input1.txt");
+
+            // PART 1 
+            int measurements = 0;
+            for (int i = 0; i < lineList.Length - 1; i++)
+            {
+                int measurement1 = int.Parse(lineList[i]);
+                int measurement2 = int.Parse(lineList[i + 1]);
+                if (measurement2 > measurement1)
+                    measurements++;
+            }
+            Console.WriteLine($"{measurements} measurements are larger than the previous measurement");
+
+            // PART 2 
+            int sums = 0;
+            int currSum = 0;
+            int prevSum;
+            for (int i = 0; i < lineList.Length - 2; i++)
+            {
+                prevSum = currSum;
+                int measurement1 = int.Parse(lineList[i]);
+                int measurement2 = int.Parse(lineList[i + 1]);
+                int measurement3 = int.Parse(lineList[i + 2]);
+                currSum = measurement1 + measurement2 + measurement3;
+                if (prevSum != 0 && currSum > prevSum)
+                    sums++;
+            }
+            Console.WriteLine($"{sums} sums are larger than the previous sum");
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# adventofcode.2021.1
-Advent of Code 2021 – Day 1: Sonar Sweep
+# Advent of Code 2021 – Day 1: Sonar Sweep
+
+This is how I solved [Advent of Code 2021 – Day 1: Sonar Sweep](https://adventofcode.com/2021/day/1) 
+
+The solution consists of a console application written in C# based on the Microsoft .NET Framework 4.6 
+
